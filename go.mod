e25b5bc999235b49e65dc2b1b0ce73d90c869899
Joword <23089538@qq.com>
2023-06-06 19:04:14 +0800

    init project

---
+++ go.mod
@@ -0,0 +1,15 @@
+module zackpop
+
+go 1.20
+
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/jinzhu/gorm v1.9.16
+	github.com/spf13/viper v1.16.0
+)
+
+require (
+	github.com/go-playground/validator/v10 v10.14.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/spf13/viper"
+)
+
+func initConfig()  {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("")
+	}
+}
+
+func main(){
+	InitConfig()
+	db := common.InitDB()
+	defer db.Close()
+
+	r := gin.Default()
+	r = CollectRoute(r)
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
+	panic(r.Run())
+}
